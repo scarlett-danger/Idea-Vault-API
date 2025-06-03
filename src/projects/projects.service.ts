59f8beeaea227c08197e400acd0453d33b3ae160
Scarlett Danger <scarlett.danger@aol.com>
2025-06-02 21:31:39 -0500

    adding project module, service and dto

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,10 +1,32 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { CreateProjectDto } from './dto/create-project.dto';
 
 @Injectable()
 export class ProjectsService {
   constructor(@Inject('POSTGRES_POOL') private readonly sql: any) {}
 
-  async findAll(): Promise<any[]> {
-    return await this.sql`SELECT * FROM projects`;
+  async createProject(dto: CreateProjectDto): Promise<any> {
+    const { projectCode, description, productLine, wantsNotifications, notificationType } = dto;
+
+    // Store notificationType as an array (Postgres supports text[]), or serialize as JSON if needed
+    const result = await this.sql`
+      INSERT INTO projects (
+        project_code,
+        description,
+        product_line,
+        wants_notifications,
+        notification_type
+      )
+      VALUES (
+        ${projectCode},
+        ${description},
+        ${productLine},
+        ${wantsNotifications},
+        ${notificationType}
+      )
+      RETURNING *;
+    `;
+    return result[0];
   }
 }
+
